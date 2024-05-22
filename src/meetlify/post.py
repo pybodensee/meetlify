1d5187bcf8d0714e35e3ba60dad7e99cf219756d
Faisal Shahzad <seowingsorg@gmail.com>
2024-05-22 17:16:04 +0200

    Included Post Data Class

---
+++ src/meetlify/post.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+"""
+Meetlify: Static Site Generator for Meetup Websites
+A Python Package for Generating Static Website for Meetups.
+https://github.com/pybodensee/meetlify
+
+    src\meetlify\post.py
+
+    Copyright (C) 2024-2024 Faisal Shahzad <info@serpwings.com>
+
+<LICENSE_BLOCK>
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+</LICENSE_BLOCK>
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+# STANDARD LIBARY IMPORTS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import codecs
+from dataclasses import dataclass
+from datetime import datetime, timezone
+from pathlib import Path
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 3rd PARTY LIBRARY IMPORTS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import markdown
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPLEMENATIONS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+@dataclass
+class Post:
+    """Post Data Class"""
+
+    title: str
+    date: str
+    modifieddate: str
+    author: str
+    slug: str
+    status: str
+    tags: str
+    description: str
+    content: str
+
+    @classmethod
+    def from_markdown(cls, page_):
+        """Genreate Post Data Class from Markdown File
+
+        Args:
+            page_ (dict): Post as dict file
+
+        Returns:
+            Post: Return Constructed Post Object
+        """
+        _md = markdown.Markdown(extensions=["meta", "attr_list"])
+        with codecs.open(page_, "r", encoding="utf-8") as f:
+            data = f.read()
+
+            return cls(
+                content=_md.convert(data),
+                date="".join(_md.Meta["date"]),
+                modifieddate=datetime.fromtimestamp(
+                    Path(page_).stat().st_mtime, tz=timezone.utc
+                ),
+                author="".join(_md.Meta["author"]),
+                title="".join(_md.Meta["title"]),
+                description="".join(_md.Meta["description"]),
+                slug="".join(_md.Meta["slug"]),
+                tags=_md.Meta["tags"],
+                status="".join(_md.Meta["status"]),
+            )
